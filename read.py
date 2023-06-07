386fbfc1642e8da9ac9e4c32482bcf6cf98bdb96
P.Y.1 <gtogju@gmail.com>
2023-06-07 18:34:14 +0800

    calc avg reviews

--- read.py
+++ read.py
@@ -7,8 +7,11 @@ with open ('reviews.txt', 'r') as f:
 		if count % 100000 == 0:
 			print(len(data))
 
-print(data[0])
-print('----------------------')
-print(data[1])
 
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sun_len = 0
+for d in data:
+	sun_len += len(d)
+print('留言平均長度是', sun_len / len(data))
 
